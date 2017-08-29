aa060dd38ce73041c5ed11f28207bbf43b6b303d
levg34 <levg34@users.noreply.github.com>
2017-08-29 21:35:03 +0200

    add up function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
 const functions = require('firebase-functions')
+const cors = require('cors')({origin: true})
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-	response.send("Hello from Firebase!")
+exports.up = functions.https.onRequest((request, response) => {
+	cors(request, response, () => {
+		if (true) {
+			response.send({code: 200})
+		} else {
+			response.status(500).send({error: 'An error occured'})
+		}
+	})
 })
