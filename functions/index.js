a218c7783de4ff61eaa397c065edc10e7ae67af2
levg34 <levg34@users.noreply.github.com>
2017-08-29 22:29:38 +0200

    log in database

--- functions/index.js
+++ functions/index.js
@@ -4,9 +4,9 @@ admin.initializeApp(functions.config().firebase)
 const cors = require('cors')({origin: true})
 
 exports.up = functions.https.onRequest((request, response) => {
-	admin.database().ref('/check/'+url).push({date: new Date()})
 	cors(request, response, () => {
 		var url = 'self'
+		admin.database().ref('/check/'+url).push({date: new Date()})
 		if (true) {
 			response.send({code: 200})
 		} else {
