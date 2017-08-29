3bd08e2c3aa0d3bcb0562825d695afda2de26385
levg34 <guilhem.dross@laposte.net>
2017-08-29 22:06:34 +0200

    log in database

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,16 @@
 const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase)
 const cors = require('cors')({origin: true})
 
 exports.up = functions.https.onRequest((request, response) => {
 	cors(request, response, () => {
+		var url = 'self'
 		if (true) {
 			response.send({code: 200})
 		} else {
 			response.status(500).send({error: 'An error occured'})
 		}
+		admin.database().ref('/check/'+url).push({date: new Date()})
 	})
 })
