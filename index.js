030840ebe3682f62f0c0addc90db7aa00b54e677
levg34 <guilhem.dross@laposte.net>
2017-08-30 16:37:34 +0200

    CORS

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
 var express = require('express') 
 var app = express() 
-const cors = require('cors')({origin: true})
 var moment = require('moment')
 var request = require('request')
 
 app.set('port', (process.env.PORT || 5000)) 
 app.use(express.static(__dirname + '/public')) 
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/up', function (req, res) {
-	cors(req, res, () => {
-		var url = req.query.url
-		var okerror = 'error'
-		const date = moment().format('DD/MM/YYYY HH:mm')
-		if (!url) {
-			okerror = 'ok'
-			res.send({code: 200})
-		} else {
-			request(url, function (error, _response, body) {
-				var resobject = {}
-				if (error) {
-					okerror = 'error'
-					resobject.error = error
+	var url = req.query.url
+	var okerror = 'error'
+	const date = moment().format('DD/MM/YYYY HH:mm')
+	if (!url) {
+		okerror = 'ok'
+		res.send({code: 200})
+	} else {
+		request(url, function (error, _response, body) {
+			var resobject = {}
+			if (error) {
+				okerror = 'error'
+				resobject.error = error
+			}
+			if (_response) {
+				resobject.code = _response.statusCode
+				if (resobject.code == 200) {
+					okerror = 'ok'
 				}
-				if (_response) {
-					resobject.code = _response.statusCode
-					if (resobject.code == 200) {
-						okerror = 'ok'
-					}
-				}
-				res.send(resobject)
-			})
-		}
-	})
+			}
+			res.send(resobject)
+		})
+	}
 })
 
 app.listen(app.get('port'), function() {
